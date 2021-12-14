8c69c0223705432578f4a4da498ed61110edc3c7
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-12-14 23:52:18 +0300

    feature: added playlist from 3 videos

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 'use strict';
+let position = 0;
+let playlist;
+let video;
+
+window.onload = function () {
+    playlist = [
+        'video/preroll',
+        'video/areyoupopular',
+        'video/destinationearth'
+    ];
+
+    video = document.getElementById('video');
+    video.addEventListener('ended', nextVideo, false);
+    playVideo(position);
+};
+
+function nextVideo() {
+    position++;
+
+    if (position === playlist.length) {
+        position = 0;
+    }
+
+    playVideo(position);
+}
+
+function playVideo(position) {
+    video.src = `${playlist[position]}${getFormatExtension()}`;
+
+    video.onloadeddata = function () {
+        console.log("Browser has loaded the current frame");
+        console.log(`Playing ${video.currentSrc}`);
+    };
+
+    video.play();
+}
+
+function getFormatExtension() {
+    if (video.canPlayType('video/webm') !== '') {
+        return '.webm';
+    } else if (video.canPlayType('video/mp4') !== '') {
+        return '.mp4';
+    } else if (video.canPlayType('video/ogg') !== '') {
+        return '.ogv';
+    } else {
+        alert('Sorry, your browser doesn\'t support the video element');
+    }
+}
